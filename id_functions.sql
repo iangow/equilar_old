84a2a5ed5c211dd5d04c087a7bec020ff10ff1a9
Ian Gow <igow@hbs.edu>
2016-02-10 08:59:01 -0500

    Added function to extract complete equilar_director_id.

--- id_functions.sql
+++ id_functions.sql
@@ -3,22 +3,34 @@ CREATE OR REPLACE FUNCTION director.director_id(text)
 $BODY$
     SELECT CASE WHEN $1 != ''
     THEN regexp_replace($1, E'^.*\\.(\\d+)$', E'\\1')::integer
-    ELSE NULL END
+    END
   $BODY$ LANGUAGE sql IMMUTABLE;
 
 COMMENT ON FUNCTION director.director_id(text) 
-    IS 'CREATED WITH ~/Dropbox/data/equilar/id_functions.sql';
+    IS 'CREATED WITH id_functions.sql FROM https://github.com/iangow/equilar/';
 
 CREATE OR REPLACE FUNCTION director.equilar_id(text)
   RETURNS integer AS
 $BODY$
     SELECT CASE WHEN $1 != ''
     THEN regexp_replace($1, E'^(\\d+)\\..*$', E'\\1')::integer
-    ELSE NULL END
+    END
 $BODY$ LANGUAGE sql IMMUTABLE;
 
 COMMENT ON FUNCTION director.equilar_id(text) 
-    IS 'CREATED WITH ~/Dropbox/data/equilar/id_functions.sql';
+    IS 'CREATED WITH id_functions.sql FROM https://github.com/iangow/equilar/';
+
+CREATE OR REPLACE FUNCTION director.equilar_director_id(text)
+  RETURNS equilar_director_id AS
+$BODY$
+    SELECT CASE WHEN $1 != ''
+    THEN (director.equilar_id($1), 
+            director.director_id($1))::equilar_director_id 
+    END
+$BODY$ LANGUAGE sql IMMUTABLE;
+
+COMMENT ON FUNCTION director.equilar_director_id(text) 
+    IS 'CREATED WITH id_functions.sql FROM https://github.com/iangow/equilar/';
 
   
 
